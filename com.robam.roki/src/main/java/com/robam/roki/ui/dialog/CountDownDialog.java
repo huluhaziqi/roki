06f992776a13326b2a2a8348c8be439ef8e7caf9
gu <1946938905@qq.com>
2016-02-26 09:08:25 +0800

    消毒提示弹框提示界面

--- com.robam.roki/src/main/java/com/robam/roki/ui/dialog/CountDownDialog.java
+++ com.robam.roki/src/main/java/com/robam/roki/ui/dialog/CountDownDialog.java
@@ -4,7 +4,10 @@ package com.robam.roki.ui.dialog;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.legent.ui.ext.BaseActivity;
 import com.robam.roki.ui.FormKey;
@@ -25,5 +28,17 @@ public class CountDownDialog extends BaseActivity {
     protected void onCreate(Bundle savedState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedState);
+
+        //设置弹框居中显示
+        Window window = getWindow();
+        window.setGravity(Gravity.CENTER);
+        //获取屏幕宽、高
+        WindowManager m = getWindowManager();
+        Display d = m.getDefaultDisplay();
+        //设置对话框当前的参数值
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.height = (int) (d.getHeight() * 0.4);   //高度设置为屏幕的1.0
+        lp.width = (int) (d.getWidth() * 1.0);    //宽度设置为屏幕的0.8
+        window.setAttributes(lp);     //设置生效
     }
 }
